5b28a66a570592136f1a281d87bb9a393cae89f8
dgknylz <162124054+dgknylz@users.noreply.github.com>
2024-03-04 14:58:37 +0300

    Update Main.java

--- nesne/src/Main.java
+++ nesne/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String[] students = {"Ali", "Ayşe", "Mehmet", "Fatma", "Mustafa"};
-        int[] times = {210, 190, 220, 180, 200};
+        String[] students = {"Kadir", "Gökhan","Hakan", "Suzan","Pınar","Mehmet","Ali","Emel","Fırat","James","Jale","Ersin","Deniz","Gözde","Anıl","Burak","Fatma", "Mustafa"};
+        int[] times = {341,273, 278, 329, 445,402,388,270,243,334,412,393,299,343,317,265};
 
         Marathon manager = new Marathon(students, times);
 
